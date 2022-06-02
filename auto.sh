99cd11f3a44b1328c16d8902990d76f9efc6cf89
bmlsj <gaga2353@gmail.com>
2022-06-02 20:23:32 +0900

    auto test

--- auto.sh
+++ auto.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env sh
 
+echo "git pull"
 git pull origin main
 
-echo "> pull complete"
-
+echo "run python"
 python test.py
 
+echo "git add"
 git add .
 
-echo "> add all files"
 
 if [ $# -eq 0 ]; then
-  # read -p "> enter commit message : " COMMIT_MESSAGE
   COMMIT_MESSAGE="auto"
 else 
   COMMIT_MESSAGE=${1}
 fi
 
-git commit -m "${COMMIT_MESSAGE}"
 
+git commit -m "${COMMIT_MESSAGE}"
 echo "> commit complte"
 
-git push origin HEAD:main
 
+git push origin HEAD:main
 echo "> push complete"
